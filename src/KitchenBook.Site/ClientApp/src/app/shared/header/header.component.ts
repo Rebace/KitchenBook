b9cbd4b2d9b05a9b9975fbe6d4d6ee12de6cd64e
Rebace <Rebace@mail.ru>
2022-06-15 21:59:38 +0300

    Добавил форму авторизации и регистрации и насторил их и аутентификацию на стороне фронта

---
+++ src/KitchenBook.Site/ClientApp/src/app/shared/header/header.component.ts
@@ -0,0 +1,40 @@
+﻿import {Component, Input, NgModule} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {UserService} from '../services/accountService';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
+})
+
+
+export class HeaderComponent {
+    public formLogin = false;
+    public formPassword = false;
+    public userName = '';
+
+    constructor(private userService: UserService) {
+    }
+    public onInit() {
+        this.userService.authentication().subscribe(
+            (result) => {
+                this.userName = <string>result;
+            }
+        );
+    }
+    public onSubmitLogin(): void {
+        this.formPassword = false;
+        this.formLogin = true;
+    }
+
+    public onSubmitRegister(): void {
+        this.formPassword = true;
+        this.formLogin = false;
+    }
+
+    public onSubmitClose(): void {
+        this.formPassword = false;
+        this.formLogin = false;
+    }
+}
---
+++ src/KitchenBook.Site/ClientApp/src/app/shared/login/login.component.ts
@@ -0,0 +1,57 @@
+﻿import {Component, EventEmitter, Output} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {UserService} from '../services/accountService';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+})
+
+export class LoginComponent {
+    @Output() public formClose = new EventEmitter();
+    @Output() public formPassword = new EventEmitter();
+    public loginForm!: FormGroup;
+    public firstDispatch: Boolean = true;
+    public successfulLogin: Boolean = true;
+
+    constructor(private fb: FormBuilder, private userService: UserService) {
+        this._createForm();
+    }
+
+    private _createForm() {
+        this.loginForm = this.fb.group({
+            'login': new FormControl('', [Validators.required]),
+            'password': new FormControl('', [Validators.required])
+        });
+    }
+
+    public onSubmitRegister(): void {
+        this.formPassword.emit();
+    }
+
+    public onSubmitClose(): void {
+        this.formClose.emit();
+    }
+
+    public login(): void {
+        this.firstDispatch = false;
+        if (this.loginForm.invalid) {
+            return;
+        }
+
+        const authenticate = {
+            login: this.loginForm.controls['login'].value,
+            password: this.loginForm.controls['password'].value,
+            name: ''
+        };
+        this.userService.login(authenticate).subscribe(
+            (result) => {
+                window.location.reload();
+            },
+            (error) => {
+                this.successfulLogin = false;
+            }
+        );
+    }
+}
