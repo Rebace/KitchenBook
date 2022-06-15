38fb023c46139140977399ab36364819058bf13a
Rebace <Rebace@mail.ru>
2022-06-15 21:45:48 +0300

    Добавил BE для аутентификации

---
+++ src/KitchenBook.Api/ApiMessages/UserModel/UserMapper.cs
@@ -0,0 +1,20 @@
+﻿using KitchenBook.Api.MessageContracts.UserModel;
+using KitchenBook.Domain.UserModel;
+
+namespace KitchenBook.Api.ApiMessages.UserModel;
+
+public static class UserMapper
+{
+    public static UserDto Map(this User user)
+    {
+        return new UserDto()
+        {
+            Name = user.Name,
+            Login = user.Login,
+            Password = user.Password,
+            Description = user.Description,
+            Id = user.Id,
+            Token = user.Token
+        };
+    }
+}
---
+++ src/KitchenBook.Infrastructure/Auxiliary/AppendToken.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace KitchenBook.Infrastructure.Auxiliary;
+
+public static class AddingToken
+{
+    public static void AppendTokenToCookies(this HttpResponse HttpContext, string token)
+    {
+        HttpContext.Cookies.Append("Token", token);
+    }
+}
