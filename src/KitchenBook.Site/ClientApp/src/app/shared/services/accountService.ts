b950b510302b81978ca3a5e4878a060af3db804f
Rebace <Rebace@mail.ru>
2022-06-19 10:56:27 +0300

    Изменил наименование для авторизации в сервисе

--- src/KitchenBook.Site/ClientApp/src/app/shared/services/accountService.ts
+++ src/KitchenBook.Site/ClientApp/src/app/shared/services/accountService.ts
@@ -4,13 +4,13 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class UserService {
-    private _userControllerLink = 'http://localhost:5000/user/';
+    private _userControllerLink = 'http://localhost:5000/users/';
 
     constructor(private http: HttpClient) {
     }
 
-    public authentication() {
-        return this.http.get(`${this._userControllerLink}authentication`);
+    public authorization() {
+        return this.http.get(`${this._userControllerLink}authorization`);
     }
 
     public login(login: Authenticate) {
