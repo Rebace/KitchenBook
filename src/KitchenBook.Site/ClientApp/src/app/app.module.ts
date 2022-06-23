032a592ccbb535c640b86540c3768413746ce1e2
Rebace <Rebace@mail.ru>
2022-06-23 15:36:48 +0300

    Добавил FE для добавления рецептов

--- src/KitchenBook.Site/ClientApp/src/app/app.module.ts
+++ src/KitchenBook.Site/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import {LoginComponent} from './shared/login/login.component';
 import {RegisterComponent} from './shared/register/register.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {UserService} from './shared/services/accountService';
+import {RecipeAddModule} from './recipe-add/recipe-add.module';
 
 @NgModule({
     declarations: [
@@ -22,6 +23,7 @@ import {UserService} from './shared/services/accountService';
     imports: [
         BrowserModule,
         RecipeListModule,
+        RecipeAddModule,
         BrowserAnimationsModule,
         AppRoutingModule,
         FormsModule,
---
+++ src/KitchenBook.Site/ClientApp/src/app/recipe-add/recipe-add-routing.module.ts
@@ -0,0 +1,19 @@
+﻿import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {RecipeAddComponent} from './recipe-add.component';
+
+const routes: Routes = [
+    {
+        path: 'recipe-add',
+        component: RecipeAddComponent,
+        children: []
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class RecipeAddRoutingModule {
+}
